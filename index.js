6cf5891b93c7e5b1f471ff9a537dde7226949784
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-03-24 20:41:37 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const studentRouter = require("./routers/student.router");
+const mentorRouter = require("./routers/mentor.router");
+const assignstudentmentorRouter = require("./routers/assignStudtoMentor.router");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER_NAME}:${process.env.password}@assigningmaster.0cygvrq.mongodb.net/Class`
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  });
+
+app.use("/students", studentRouter);
+app.use("/mentors", mentorRouter);
+app.use("/assign-student-mentor", assignstudentmentorRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my server!!!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
